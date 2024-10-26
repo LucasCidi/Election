5d6b9d9be9614fbbdd41463201e85815a5ca7c35
Gaeltomaz123 <gaeltomaz@gmail.com>
2024-10-26 19:06:21 -0300

    falta arrumar umas coisas

--- ring_base_code_v2.go
+++ ring_base_code_v2.go
@@ -5,12 +5,16 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 type mensagem struct {
 	tipo  int    // tipo da mensagem para fazer o controle do que fazer (eleição, confirmacao da eleicao)
-	corpo [3]int // conteudo da mensagem para colocar os ids (usar um tamanho ocmpativel com o numero de processos no anel)
+	corpo [4]int // conteudo da mensagem para colocar os ids (usar um tamanho ocmpativel com o numero de processos no anel)
+	vencedor int
 }
 
 var (
@@ -33,64 +37,116 @@ func ElectionControler(in chan int) {
 
 	// mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isto)
 
-	temp.tipo = 2
+	temp.tipo = 3
 	chans[3] <- temp
-	fmt.Printf("Controle: mudar o processo 0 para falho\n")
 
-	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
+	time.Sleep(6 * time.Second)
 
-	// mudar o processo 1 - canal de entrada 0 - para falho (defini mensagem tipo 2 pra isto)
-
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	numeroAleatorio := r.Intn(4) // Gera um número entre 0 e 3
 	temp.tipo = 2
-	chans[0] <- temp
-	fmt.Printf("Controle: mudar o processo 1 para falho\n")
-	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
+	chans[numeroAleatorio] <- temp
 
-	// matar os outrs processos com mensagens não conhecidas (só pra cosumir a leitura)
+	for {
+		time.Sleep(4 * time.Second)
+		control := <-in 
 
-	temp.tipo = 4
-	chans[1] <- temp
-	chans[2] <- temp
+		// Nova eleicao
+		if(control == 1) {
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			numeroAleatorio := r.Intn(4)
+			temp.tipo = 1
+			chans[numeroAleatorio] <- temp
+		}
 
-	fmt.Println("\n   Processo controlador concluído\n")
+		// Nova falha
+		if(control == 3) {
+			time.Sleep(2 * time.Second)
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			numeroAleatorio := r.Intn(4) 
+			temp.tipo = 2
+			chans[numeroAleatorio] <- temp
+		}
+	
+	}
 }
 
 func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) {
 	defer wg.Done()
-
-	// variaveis locais que indicam se este processo é o lider e se esta ativo
-
-	var actualLeader int
 	var bFailed bool = false // todos inciam sem falha
-
+	// variaveis locais que indicam se este processo é o lider e se esta ativo
+	var actualLeader int
 	actualLeader = leader // indicação do lider veio por parâmatro
 
-	temp := <-in // ler mensagem
-	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+	for {
+		temp := <-in // ler mensagem
+		switch temp.tipo {
+		// Eleicao
+		case 1:
+			{
+				time.Sleep(2 * time.Second)
+				if !bFailed { // Se eu nao falhei posso participar da eleicao
+					if temp.corpo[TaskId-1] != TaskId { // Se ainda nao deu a volta no anel
+						temp.corpo[TaskId-1] = TaskId
+						fmt.Printf("%2d: ELEIÇÃO: [ %d, %d, %d, %d ]\n", TaskId, temp.corpo[0], temp.corpo[1], temp.corpo[2], temp.corpo[3])
+						out <- temp
+					} else { // Se deu a volta no anel
+						menorValor := math.MaxInt // Inicializa com o maior valor possível
+						for _, valor := range temp.corpo {
+							if valor < menorValor && valor != 0{
+								menorValor = valor
+							}
+						}
+						temp.vencedor = menorValor
+						temp.tipo = 3
+						temp.corpo[0] = 0
+						temp.corpo[1] = 0
+						temp.corpo[2] = 0
+						temp.corpo[3] = 0
+						out <- temp
+						//controle <- 2
+					}
+					} else {
+						fmt.Printf("%2d: Estou falho não posso pariticpar", TaskId)
+						out <- temp
+					}
+			}
 
-	switch temp.tipo {
-	case 2:
-		{
-			bFailed = true
-			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
-			controle <- -5
-		}
-	case 3:
-		{
-			bFailed = false
-			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
-			controle <- -5
-		}
-	default:
-		{
-			fmt.Printf("%2d: não conheço este tipo de mensagem\n", TaskId)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+		// Falha
+		case 2:
+			{
+				time.Sleep(2 * time.Second)
+				bFailed = true
+				fmt.Printf("%2d: falhei %v \n", TaskId, bFailed)
+				if TaskId == actualLeader {
+					fmt.Printf("%2d: Sou o lider? Sim\n", TaskId)
+					controle <- 1
+				} else {
+					fmt.Printf("%2d: Sou o lider? Não\n", TaskId)
+					temp.tipo = 3
+					controle <- 3
+					out <- temp
+				}
+			}
+
+		// Roda Processo
+		case 3:
+			{
+				time.Sleep(2 * time.Second)
+				if actualLeader != temp.vencedor && temp.vencedor != 0{
+					actualLeader = temp.vencedor
+				}
+				if !bFailed {
+					fmt.Printf("%2d: Rodando, Líder atual: %d\n", TaskId, actualLeader)
+				} else {
+					fmt.Printf("%2d: Estou Falho\n", TaskId)
+				}
+				out <- temp
+			}
+		
+		fmt.Printf("%2d: Terminei \n", TaskId)
 		}
 	}
-
-	fmt.Printf("%2d: terminei \n", TaskId)
 }
 
 func main() {
@@ -99,10 +155,10 @@ func main() {
 
 	// criar os processo do anel de eleicao
 
-	go ElectionStage(0, chans[3], chans[0], 0) // este é o lider
-	go ElectionStage(1, chans[0], chans[1], 0) // não é lider, é o processo 0
-	go ElectionStage(2, chans[1], chans[2], 0) // não é lider, é o processo 0
-	go ElectionStage(3, chans[2], chans[3], 0) // não é lider, é o processo 0
+	go ElectionStage(1, chans[3], chans[0], 1) // este é o lider
+	go ElectionStage(2, chans[0], chans[1], 1) // não é lider, é o processo 0
+	go ElectionStage(3, chans[1], chans[2], 1) // não é lider, é o processo 0
+	go ElectionStage(4, chans[2], chans[3], 1) // não é lider, é o processo 0
 
 	fmt.Println("\n   Anel de processos criado")
 
